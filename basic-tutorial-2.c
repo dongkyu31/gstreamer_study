3a4c4d49242a81ae0d3eedb7f9d0ac4c7440b4c6
dongkyu31 <dongkyu31@gmail.com>
2022-05-13 14:44:26 +0900

    upload basic-tutorial-3.c and modify 2.c

--- basic-tutorial-2.c
+++ basic-tutorial-2.c
@@ -1,3 +1,9 @@
+
+/*
+ * Basic tutorial 2 : GStreamer concepts
+ */
+
+
 #include <gst/gst.h>
 
 int main(int argc, char * argv[])
@@ -11,10 +17,12 @@ int main(int argc, char * argv[])
 	gst_init(&argc, &argv);
 
 	/* Create the elements */
+	// gst_element_factory_make() 함수를 통해서 element를 생성한다.
 	source = gst_element_factory_make("videotestsrc", "source");
 	sink = gst_element_factory_make("autovideosink", "sink");
 
 	/* Create the empty pipeline */
+	// gst_pipeline_new() 함수를 통해서 빈 pipeline을 생성한다.
 	pipeline = gst_pipeline_new("test-pipeline");
 
 	if (!pipeline || !source || !sink) {
@@ -24,8 +32,11 @@ int main(int argc, char * argv[])
 
 
 	/* Build the pipeline */
-	gst_bin_add_many (GST_BIN (pipeline), source, sink, NULL);
-	if (gst_element_link (source, sink) != TRUE) {
+	// pipeline을 구성하기 위해 gst_bin_add_many() 함수를 이용해 pipeline에 element를추가하고,
+	// gst_element_link() 함수를 이용해서 element 간에 link를 생성한다.
+	// link 전에 link될 element들은 반드시 같은 pipeline에 추가시겨야 한다.
+	gst_bin_add_many(GST_BIN(pipeline), source, sink, NULL);
+	if (gst_element_link(source, sink) != TRUE) {
 		g_printerr("Elements could not be linked.\n");
 		gst_object_unref(pipeline);
 		return -1;
@@ -33,6 +44,9 @@ int main(int argc, char * argv[])
 
 
 	/* Modify the source's properties */
+	// 대부분의 GStreamer element는 행동을 바꿀 수 있는 writable property와 상태를 알 수 있는 readable property를 가지고 있다.
+	// 그래서 g_object_get()으로 속성 값을 가져오고 g_object_set()을 통해서 속성값을 입력할 수 있다.
+	// 만약, element에서 사용될 수 있는 property의 값들을 알고 싶다면, gst-inspect-1.0 툴을 사용하면 된다.
 	g_object_set (source, "pattern", 0, NULL);
 
 	/* Start playing */
